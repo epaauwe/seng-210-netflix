8de3108da0efd5aac230e7ef48d2441567b6f4d0
jesse winden <winden.jesse@gmail.com>
2019-03-30 17:13:45 -0400

    Added Sign in Interface/ Added Create User Interface

--- NetflixRec/NetflixRec/MainWindow.xaml.cs
+++ NetflixRec/NetflixRec/MainWindow.xaml.cs
@@ -24,5 +24,141 @@ namespace NetflixRec
         {
             InitializeComponent();
         }
+        
+        
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            LoginPage.Visibility = Visibility.Visible;
+            LoginBorder.Visibility = Visibility.Visible;
+            UserNameBox.Foreground = Brushes.Gray;
+            PassWordBox.Foreground = Brushes.Gray;
+            UserNameBox.Text = "Insert Username Here";
+            PassWordBox.Text = "Insert Password Here";
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            SignIn.Visibility = Visibility.Hidden;
+            LoginPage.Visibility = Visibility.Hidden;
+            LoginBorder.Visibility = Visibility.Hidden;
+            SignOut.Visibility = Visibility.Visible;
+        }
+
+        private void SignOut_Click(object sender, RoutedEventArgs e)
+        {
+            SignOut.Visibility = Visibility.Hidden;
+            SignIn.Visibility = Visibility.Visible;
+        }
+
+        private void CreateUser_Click(object sender, RoutedEventArgs e)
+        {
+            CreateUserForm.Visibility = Visibility.Visible;
+            CreateUserFormBorder.Visibility = Visibility.Visible;
+            FirstNameCreatedForm.Foreground = Brushes.Gray;
+            LastNameCreatedForm.Foreground = Brushes.Gray;
+            UserNameCreatedForm.Foreground = Brushes.Gray;
+            PasswordCreatedForm.Foreground = Brushes.Gray;
+            FirstNameCreatedForm.Text = "First Name";
+            LastNameCreatedForm.Text = "Last Name";
+            UserNameCreatedForm.Text = "Username";
+            PasswordCreatedForm.Text = "Password";
+        }
+
+        private void UserNameBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            UserNameBox.Clear();
+            UserNameBox.Foreground = Brushes.Black;
+        }
+
+        private void UserNameBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(UserNameBox.Text == "")
+            {
+                UserNameBox.Foreground = Brushes.Gray;
+                UserNameBox.Text = "Insert Username Here";
+            }
+        }
+
+        private void PassWordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(PassWordBox.Text == "")
+            {
+                PassWordBox.Foreground = Brushes.Gray;
+                PassWordBox.Text = "Insert Password Here";
+            }
+            
+        }
+
+        private void PassWordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PassWordBox.Clear();
+            PassWordBox.Foreground = Brushes.Black;
+        }
+
+        private void FirstNameCreatedForm_GotFocus(object sender, RoutedEventArgs e)
+        {
+            FirstNameCreatedForm.Clear();
+            FirstNameCreatedForm.Foreground = Brushes.Black;
+        }
+
+        private void FirstNameCreatedForm_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (FirstNameCreatedForm.Text == "")
+            {
+                FirstNameCreatedForm.Foreground = Brushes.Gray;
+                FirstNameCreatedForm.Text = "First Name";
+            }
+        }
+
+        private void LastNameCreatedForm_GotFocus(object sender, RoutedEventArgs e)
+        {
+            LastNameCreatedForm.Clear();
+            LastNameCreatedForm.Foreground = Brushes.Black;
+        }
+
+        private void LastNameCreatedForm_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (LastNameCreatedForm.Text == "")
+            {
+                LastNameCreatedForm.Foreground = Brushes.Gray;
+                LastNameCreatedForm.Text = "Last Name";
+            }
+        }
+
+        private void UserNameCreatedForm_GotFocus(object sender, RoutedEventArgs e)
+        {
+            UserNameCreatedForm.Clear();
+            UserNameCreatedForm.Foreground = Brushes.Black;
+        }
+
+        private void UserNameCreatedForm_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (UserNameCreatedForm.Text == "")
+            {
+                UserNameCreatedForm.Foreground = Brushes.Gray;
+                UserNameCreatedForm.Text = "Username";
+            }
+        }
+
+        private void PasswordCreatedForm_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordCreatedForm.Clear();
+            PasswordCreatedForm.Foreground = Brushes.Black;
+        }
+
+        private void PasswordCreatedForm_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (PasswordCreatedForm.Text == "")
+            {
+                PasswordCreatedForm.Foreground = Brushes.Gray;
+                PasswordCreatedForm.Text = "Password";
+            }
+        }
+
+        private void CreateUserButton_onForm_Click(object sender, RoutedEventArgs e)
+        {
+            CreateUserForm.Visibility = Visibility.Hidden;
+            CreateUserFormBorder.Visibility = Visibility.Hidden;
+        }
     }
 }
